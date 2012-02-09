5aafb08ec2b0a2ac446a1e6cedf673d96e16057e
hammondjn <jessica@mobiusconsortium.org>
2012-02-09 12:45:35 -0600

    Set up the redirect script and cleaned up the toplogos

---
+++ domainredirect.js
@@ -0,0 +1,32 @@
+var href = window.location.href
+var edu = ".edu"
+var domainLength = href.indexOf(edu) + edu.length
+var notDomain = href.substring(domainLength)
+var searchmobius = 'http://lance.searchmobius.org'
+
+if (notDomain == "/") {
+  var redirectUrl = searchmobius  + "/search~S0&redirected"  
+}else {
+  var r =  /\W$/
+  if (r.test(notDomain) == true){
+    notDomain = notDomain.slice(0,-1);
+  }
+  redirectUrl = searchmobius + notDomain + "&redirected"
+}
+
+if(href.indexOf("missouri.edu") != -1 ){
+    var oldDomain = "lance.missouri.edu"
+    window.location = redirectUrl
+}else if(href.indexOf("umsystem.edu") != -1 ){
+    var oldDomain = "lance.missouri.edu"
+    window.location = redirectUrl; 
+}else{}
+
+if (href.indexOf("&redirected") != -1 ) {
+    var redirectMessage = "<p>You've been redirected from lance.mobius.umsystem.edu or lance.missouri.edu. Starting in June of 2012, those addresses will no longer work.</p><p>Please update your bookmarks to <a href='http://lance.searchmobius.org'>http://lance.searchmobius.org</a>.</p>"
+
+    $(document).ready(function () {
+        $('#redirectMessage').html(redirectMessage);
+    });  
+}
+
