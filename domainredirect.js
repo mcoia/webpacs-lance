ee31d9a6aedcd1e871909637ac0070f12faae997
hammondjn <jessica@mobiusconsortium.org>
2012-03-02 09:56:14 -0600

    Changed the domain redirect script to use the alertMessage div

--- domainredirect.js
+++ domainredirect.js
@@ -26,7 +26,7 @@ if (href.indexOf("&redirected") != -1 ) {
     var redirectMessage = "<p>You've been redirected from lance.mobius.umsystem.edu or lance.missouri.edu. Starting in June of 2012, those addresses will no longer work.</p><p>Please update your bookmarks to <a href='http://lance.searchmobius.org'>http://lance.searchmobius.org</a>.</p>"
 
     $(document).ready(function () {
-        $('#redirectMessage').html(redirectMessage);
+        $('#alertMessage').html(redirectMessage);
     });  
 }
 
