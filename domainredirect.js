d14c02c6dd42836d5c9254d8c0e7842a28ab09cf
hammondjn <jessica@mobiusconsortium.org>
2012-05-04 11:49:47 -0500

    Updated the domain redirect message

--- domainredirect.js
+++ domainredirect.js
@@ -42,10 +42,10 @@ if (scope == undefined || scope == 0){
 
 if (href.indexOf("&redirected") != -1 ) {
     
-    var redirectMessage = "<p>You've been redirected from lance.mobius.umsystem.edu or lance.missouri.edu. Starting in June of 2012, those addresses will no longer work.</p><p>Please update your bookmarks to <a href='" + newURL + "'>" + newURL + "</a>.</p>"
+    var redirectMessage = "<p>You've been redirected from lance.mobius.umsystem.edu or lance.missouri.edu. Starting on May 22, 2012, those addresses will no longer work.</p><p>Please update your bookmarks to <a href='" + newURL + "'>" + newURL + "</a>.</p>"
 
     $(document).ready(function () {
-        $('#redirectMessage').html(redirectMessage);
+        $('#alertMessage').html(redirectMessage);
     });  
 }
 
