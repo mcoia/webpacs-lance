ad360b8895cc0c7c4ef6d9204c630c748bc03bb8
Justin Hopkins <hopkinsju@gmail.com>
2016-01-14 16:52:47 -0600

    resetting this branch to the working set of files from staging.
    At this point in the project things were in two separate repos. This branch looks to have been initially used to bring in the Dayton files, but then quickly forgotten. Since there wasn't a single branch in either this or the responsive_example_set repo that represented the Truman specific responsiveness, I decided to repurpose this branch based purely on name.

---
+++ 856links.js
@@ -0,0 +1,36 @@
+function hideLinks(){
+    
+    var stringsToHide = new Array ();
+    stringsToHide[1] = new Array ("culver");
+    stringsToHide[2] = new Array ("hlg");
+    stringsToHide[3] = new Array ("atsu");
+    stringsToHide[4] = new Array ("State Tech");
+    stringsToHide[5] = new Array ("MACC");
+    stringsToHide[6] = new Array ("Truman", "truman");
+
+
+
+    var scopeDropdown = $("#searchscope").val();
+    
+    delete stringsToHide[scopeDropdown];
+	
+    if ($("table").is('.bibLinks')){
+	for (var k in stringsToHide) {
+	    for (var l in stringsToHide[k]) {
+		$(".bibLinks a").each(function(index, element) {
+		    if($(element).parent().html().indexOf(stringsToHide[k][l]) > -1) {
+			$(element).parent().parent().hide()
+		    }    
+		});
+	    }
+	}
+	if ($(".bibLinks tr[style*=none]").length < $(".bibLinks a").length){
+	    $(".bibLinks").show();       
+	}
+    }  
+}
+    
+    
+    $(document).ready(function () {
+	hideLinks();
+});
---
+++ culverstock_encore.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function(){
+	jQuery('span .addtlInfoItem:not(:contains("Culver-Stockton"))').addClass('hideMe');
+	jQuery(".hideMe").hide();
+});
