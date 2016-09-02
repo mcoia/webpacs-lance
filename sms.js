0d400558fa4511cfc73c17701b0bae77da43f3cd
hackjess <jmh5188@truman.edu>
2016-09-02 11:36:59 -0500

    turn back on sms features with some script change

--- sms.js
+++ sms.js
@@ -31,17 +31,18 @@ try {
  out += '<input type=hidden name=title value=\"'+title+'\">';	//dump the title into a hidden form variable
  out += '<p><b>Enter your cell phone #</b>: <input name=phone type=text></p>';	// input for the phone #
  out += "<p class=eg>(use the full 10 digits of your phone #, no spaces, no dashes eg. 6105265000)</p>";
- out += "<p><b>Select your provider:</b><select name=provider>";	// pull-down for each of phone carriers the values will be parsed by the perl script
-	out += "<option value=cingular>Cingular/AT&amp;T</option>";
-	out += "<option value=cricket>Cricket</option>";
-	out += "<option value=nextel>Nextel</option>";
-	out += "<option value=northwest>Northwest Cellular</option>";
-	out += "<option value=qwest>Qwest</option>";
-	out += "<option value=sprint>Sprint</option>";
-	out += "<option value=tmobile>T-Mobile</option>";
-	out += "<option value=uscellular>US Cellular</option>";
-	out += "<option value=verizon>Verizon</option>";
-	out += "<option value=virgin>Virgin</option>";
+ out += "<p><b>Select your provider:</b><select name=provider>";	// pull-down for each of phone carriers the values will be parsed by the perl script 
+	out += "<option value="att">AT&amp;T</option>";
+	out += "<option value="cingular">Cingular</option>";
+	out += "<option value="cricket">Cricket</option>";
+	out += "<option value="nextel">Nextel</option>";
+	out += "<option value="northwest">Northwest Cellular</option>";
+	out += "<option value="qwest">Qwest</option>";
+	out += "<option value="sprint">Sprint</option>";
+	out += "<option value="tmobile">T-Mobile</option>";
+	out += "<option value="uscellular">US Cellular</option>";
+	out += "<option value="verizon">Verizon</option>";
+	out += "<option value="virgin">Virgin</option>";
  out += "</select></p>";
  out += "<p><b>Choose the item you need:</b><ol>";
  
